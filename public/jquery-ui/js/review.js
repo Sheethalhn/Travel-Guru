b4b36f928c76312f9f520a573ef886df2d63172d
aparnada7 <aparna20dalal@gmail.com>
2019-03-06 20:52:58 -0800

    Updates for review page-resizable & slider

--- public/jquery-ui/js/review.js
+++ public/jquery-ui/js/review.js
@@ -1,27 +1,45 @@
-$(init);
 
-function init()
-{
-    $("h1").addClass("ui-widget-header");
-    $("#tabs").tabs();
-    $("#datePicker").datepicker();
-    $("#slider").slider().bind("slide", reportSlider);
-    $("#selectable").selectable();
-    $("#sortable").sortable();
-    $("#dialog").dialog();
-    $("#dialog").dialog("close");
+
+$( function() {
+  var handle = $( "#custom-handle" );
+  $( "#slider" ).slider({
+    create: function() {
+      handle.text( $( this ).slider( "value" ) );
+    },
+    min: 1,
+    max: 10,
+    step: 0.5,
+    slide: function( event, ui ) {
+      handle.text( ui.value );
+    }
+  });
+} );
+
+$(init);
+function init(){
+  $("#slider").slider().bind("slide");
+  $("#resize").resizable();
+  $("p").addClass("ui-widget")
+  .addClass("ui-widget-content")
+  .addClaass("ui-corner-all");
+}
+
+function submit(){
+  $("#resize").val("");
+  $("#location").val("");
+
 }
 
 function reportSlider()
 {
     var sliderVal = $("#slider").slider("value");
     $("#slideOutput").html(sliderVal);
-} 
+}
 
 function openDialog()
 {
     $("#dialog").dialog("open");
-} 
+}
 
 function closeDialog()
 {
