2209c1bca0be3bceb7d1543c8d1436170dfdc42b
sheethalhn <sheethal.halandurnagaraja@sjsu.edu>
2019-03-08 00:20:03 -0800

    fixes:UI fixes

--- public/jquery-ui/js/visit.js
+++ public/jquery-ui/js/visit.js
@@ -8,16 +8,6 @@ function init(){
   $("#location").selectmenu();
   $("#days").spinner();
   $('#resizable').resizable();
-  if (!$.datepicker.initialized) {
-	    $(document).mousedown($.datepicker._checkExternalClick)
-	        // !!!!!!!!!!
-	        // The next code line has to be added again so that the date picker
-	        // shows up when the popup is opened more than once without reloading
-	        // the "base" page.
-	        // !!!!!!!!!!
-	        .find(document.body).append($.datepicker.dpDiv);
-	    $.datepicker.initialized = true;
-	}
 }
 
 function submit(){
