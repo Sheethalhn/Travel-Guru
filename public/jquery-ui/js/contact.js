b71bd3a9faf847c7809d2cf7c34943d7b941ccce
sheethalhn <sheethal.halandurnagaraja@sjsu.edu>
2019-03-08 13:20:44 -0800

    Submit:UI changes

--- public/jquery-ui/js/contact.js
+++ public/jquery-ui/js/contact.js
@@ -19,6 +19,31 @@ function init(){
   $("#target").bind("dropout", resetTarget);
   $("input[type='radio']").checkboxradio();
   
+  $(".dragMe").data({
+	    'originalLeft': $(".dragMe").css('left'),
+	    'origionalTop': $(".dragMe").css('top')
+	});
+  $(".dragMe1").data({
+	    'originalLeft': $(".dragMe1").css('left'),
+	    'origionalTop': $(".dragMe1").css('top')
+	});
+  $(".dragMe2").data({
+	    'originalLeft': $(".dragMe2").css('left'),
+	    'origionalTop': $(".dragMe2").css('top')
+	});
+  $(".dragMe3").data({
+	    'originalLeft': $(".dragMe3").css('left'),
+	    'origionalTop': $(".dragMe3").css('top')
+	});
+  $(".dragMe4").data({
+	    'originalLeft': $(".dragMe4").css('left'),
+	    'origionalTop': $(".dragMe4").css('top')
+	});
+  $(".dragMe5").data({
+	    'originalLeft': $(".dragMe5").css('left'),
+	    'origionalTop': $(".dragMe5").css('top')
+	});
+
 }
 
 function submit(){
@@ -30,6 +55,32 @@ function submit(){
     $("#lname").val(' ');
     $("#pno").val(' ');
     $("#mail").val(' ');
+    $("#target").removeClass("ui-state-highlight")
+    .html("Upload picture");
+    $(".dragMe").css({
+        'left': $(".dragMe").data('originalLeft'),
+        'top': $(".dragMe").data('origionalTop')
+    });
+    $(".dragMe1").css({
+        'left': $(".dragMe1").data('originalLeft'),
+        'top': $(".dragMe1").data('origionalTop')
+    });
+    $(".dragMe2").css({
+        'left': $(".dragMe2").data('originalLeft'),
+        'top': $(".dragMe2").data('origionalTop')
+    });
+    $(".dragMe3").css({
+        'left': $(".dragMe3").data('originalLeft'),
+        'top': $(".dragMe3").data('origionalTop')
+    });
+    $(".dragMe4").css({
+        'left': $(".dragMe4").data('originalLeft'),
+        'top': $(".dragMe4").data('origionalTop')
+    });    
+    $(".dragMe5").css({
+        'left': $(".dragMe5").data('originalLeft'),
+        'top': $(".dragMe5").data('origionalTop')
+    });
 }
 
 function highlightTarget(event, ui)
